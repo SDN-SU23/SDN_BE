ac0bbe4371c1c3b2917d630e36b40f4506f92d73
Huynh Chi Bao <baohc110902@gmail.com>
2024-03-10 02:30:44 +0700

    delete react

--- src/routes/react.route.js
+++ src/routes/react.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reactController = require('../controllers/react.controller');
 
 router.post('/', reactController.createNewReact);
+router.delete('/', reactController.deleteReact);
 
 module.exports = router;
