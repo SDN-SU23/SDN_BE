4e85015411e8e99d187493f34ad4b52e3ff8198e
Phạm Bá Phước <phuoc.18112002@gmail.com>
2024-03-12 17:24:55 +0700

    handle get image

--- src/services/artwork.service.js
+++ src/services/artwork.service.js
@@ -3,12 +3,13 @@
 const artworkModel = require('../models/artwork.model')
 const reactModel = require('../models/react.model')
 const commentModel = require('../models/comment.model')
-const { createSignedUrlDetail } = require('../services/upload.service');
+const { createSignedUrlDetail } = require('../services/upload.service')
 
 class ArtworkService {
     static getListArtwork = async (query) => {
         try {
-            let { searchName, categoryName, currentPage, pageSize, userId } = query
+            let { searchName, categoryName, currentPage, pageSize, userId } =
+                query
             // init filter
             let filter = {}
             filter.status = 'public'
@@ -17,9 +18,9 @@ class ArtworkService {
                 categoryName = category.split(',')
                 filter.category = { $in: categoryName }
             }
-            // check searchName 
+            // check searchName
             if (searchName) {
-                filter.title = { $regex: searchName, $options: 'i' };
+                filter.title = { $regex: searchName, $options: 'i' }
             }
             // return list of artwork
             const result = await artworkModel
@@ -27,9 +28,11 @@ class ArtworkService {
                 .limit(pageSize)
                 .skip((currentPage - 1) * pageSize)
                 .populate('authorId')
-                .lean();
+                .lean()
             // get total page
-            const totalPage = Math.ceil(await artworkModel.countDocuments(filter) / pageSize)
+            const totalPage = Math.ceil(
+                (await artworkModel.countDocuments(filter)) / pageSize
+            )
             // return filter data and total page
             const artwork = result.map((item) => {
                 return {
@@ -42,15 +45,20 @@ class ArtworkService {
                     price: item.price,
                     authorId: item.authorId,
                 }
-            });
+            })
             // get signed url of artwork
-            // for (let i = 0; i < artwork.length; i++) {
-            //     artwork[i].artworkURL = await createSignedUrlDetail(artwork[i].artworkURL)
-            // }
+            for (let i = 0; i < artwork.length; i++) {
+                artwork[i].artworkURL = await createSignedUrlDetail(
+                    artwork[i].artworkURL
+                )
+            }
             // check like of user
             if (userId) {
                 for (let i = 0; i < artwork.length; i++) {
-                    let react = await reactModel.findOne({ userId: userId, artworkId: artwork[i].artworkId })
+                    let react = await reactModel.findOne({
+                        userId: userId,
+                        artworkId: artwork[i].artworkId,
+                    })
                     if (react) {
                         artwork[i].isLike = true
                     } else {
@@ -63,9 +71,8 @@ class ArtworkService {
                 artwork,
                 currentPage: currentPage,
                 totalPage: totalPage,
-                pageSize: pageSize
-            };
-
+                pageSize: pageSize,
+            }
         } catch (error) {
             global.logger.error('Service:: getListArtwork', error)
             throw error
@@ -74,11 +81,14 @@ class ArtworkService {
 
     static getArtworkDetail = async (artworkId, userId) => {
         try {
-            let result = await artworkModel.findById(artworkId).populate('authorId').lean();
+            let result = await artworkModel
+                .findById(artworkId)
+                .populate('authorId')
+                .lean()
             if (result) {
                 result = {
                     title: result.title,
-                    imageURL: result.imageURL,
+                    // imageURL: result.imageURL,
                     description: result.description,
                     authorName: result.authorId.name,
                     authorAvatar: result.authorId.avatarUrl,
@@ -92,24 +102,30 @@ class ArtworkService {
                     commentNumber: result.commentNumber,
                     reactNumber: result.reactNumber,
                     isLike: false,
-                    // imageURL: await createSignedUrlDetail(result.imageURL)
-                    status: result.status
+                    imageURL: await createSignedUrlDetail(result.imageURL),
+                    status: result.status,
                 }
             }
             // get react of artwork
-            let reactList = await reactModel.find({ artworkId: artworkId }).populate('userId').lean();
+            let reactList = await reactModel
+                .find({ artworkId: artworkId })
+                .populate('userId')
+                .lean()
 
             if (reactList) {
                 reactList = reactList.map((item) => {
                     return {
                         authorAvatar: item.userId.avatarUrl,
                         authorName: item.userId.name,
-                        authorId: item.userId._id
+                        authorId: item.userId._id,
                     }
                 })
             }
             // get comment of artwork
-            let commentList = await commentModel.find({ artworkId: artworkId }).populate('authorId').lean();
+            let commentList = await commentModel
+                .find({ artworkId: artworkId })
+                .populate('authorId')
+                .lean()
 
             if (commentList) {
                 commentList = commentList.map((item) => {
@@ -123,7 +139,10 @@ class ArtworkService {
             }
             // check like of user
             if (userId) {
-                let react = await reactModel.findOne({ userId: userId, artworkId: artworkId })
+                let react = await reactModel.findOne({
+                    userId: userId,
+                    artworkId: artworkId,
+                })
                 if (react) {
                     result.isLike = true
                 } else {
@@ -134,7 +153,7 @@ class ArtworkService {
             return {
                 commentList,
                 artwork: result,
-                reactList
+                reactList,
             }
         } catch (error) {
             global.logger.error('Service:: getArtworkDetail', error)
@@ -144,8 +163,8 @@ class ArtworkService {
 
     static createArtwork = async (data) => {
         try {
-            const result = await artworkModel.create(data);
-            return result;
+            const result = await artworkModel.create(data)
+            return result
         } catch (error) {
             global.logger.error('Service:: createArtwork', error)
             throw error
@@ -154,7 +173,9 @@ class ArtworkService {
 
     static deleteArtwork = async (artworkId) => {
         try {
-            const result = await artworkModel.findByIdAndUpdate(artworkId, { status: 'private' });
+            const result = await artworkModel.findByIdAndUpdate(artworkId, {
+                status: 'private',
+            })
             return result
         } catch (error) {
             global.logger.error('Service:: deleteArtwork', error)
@@ -164,15 +185,13 @@ class ArtworkService {
 
     static updateArtwork = async (artworkId, data) => {
         try {
-            const result = await artworkModel
-                .findByIdAndUpdate(
-                    artworkId,
-                    data,
-                    { new: true }
-                )
+            const result = await artworkModel.findByIdAndUpdate(
+                artworkId,
+                data,
+                { new: true }
+            )
             return result
-        }
-        catch (error) {
+        } catch (error) {
             global.logger.error('Service:: updateArtwork', error)
             throw error
         }
@@ -180,10 +199,13 @@ class ArtworkService {
 
     static updateArtworkByAdmin = async (artworkId, status) => {
         try {
-            const result = await artworkModel.findByIdAndUpdate(artworkId, { status: status }, { new: true })
+            const result = await artworkModel.findByIdAndUpdate(
+                artworkId,
+                { status: status },
+                { new: true }
+            )
             return result
-        }
-        catch (error) {
+        } catch (error) {
             global.logger.error('Service:: updateArtworkByAdmin', error)
             throw error
         }
@@ -194,9 +216,9 @@ class ArtworkService {
             let { searchName, currentPage, pageSize, status } = query
             // init filter
             let filter = {}
-            // check searchName 
+            // check searchName
             if (searchName) {
-                filter.name = { $regex: searchName, $options: 'i' };
+                filter.name = { $regex: searchName, $options: 'i' }
             }
             // check status
             if (status) {
@@ -208,17 +230,18 @@ class ArtworkService {
                 .limit(pageSize)
                 .skip((currentPage - 1) * pageSize)
                 .populate('authorId')
-                .lean();
+                .lean()
             // get total page
-            const totalPage = Math.ceil(await artworkModel.countDocuments(filter) / pageSize)
+            const totalPage = Math.ceil(
+                (await artworkModel.countDocuments(filter)) / pageSize
+            )
 
             return {
                 result,
                 currentPage: currentPage,
                 totalPage: totalPage,
-                pageSize: pageSize
-            };
-
+                pageSize: pageSize,
+            }
         } catch (error) {
             global.logger.error('Service:: getArtWorkByAdmin', error)
             throw error
