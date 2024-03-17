07d6ab8d7a13afe8d8dd3579eec4f8dfea8bff02
Huynh Chi Bao <baohc110902@gmail.com>
2024-03-17 22:45:33 +0700

    Add withdraw functionality and update transaction routes

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -6,7 +6,10 @@ class TransactionController {
     withDraw = async (req, res) => {
         try {
             const response = await TransactionService.withdraw(req.body);
-            return res.status(200).json(response);
+            return res.status(200).json({
+                message: 'Withdraw successfully',
+                result: response
+            });
         } catch (error) {
             return res.status(400).json({ message: error.message });
         }
@@ -25,6 +28,36 @@ class TransactionController {
         try {
             const response = await TransactionService.getListTransaction(req.query);
             return res.status(200).json(response);
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    getListWithdrawByAdmin = async (req, res) => {
+        try {
+            const response = await TransactionService.getListWithdrawByAdmin(req.query);
+            return res.status(200).json(response);
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    getListWithdrawByUser = async (req, res) => {
+        try {
+            const response = await TransactionService.getListWithdrawByUser(req.query, req.params.userId);
+            return res.status(200).json(response);
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    updateWithdrawStatus = async (req, res) => {
+        try {
+            const response = await TransactionService.updateWithdrawStatus(req.params.id, req.body.status);
+            return res.status(200).json({
+                message: 'Update withdraw status successfully',
+                result: response
+            });
         } catch (error) {
             return res.status(400).json({ message: error.message });
         }
--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -3,12 +3,15 @@
 const transactionModel = require('../models/transaction.model');
 
 class TransactionService {
+
     static withdraw = async (detail) => {
         try {
             const response = await transactionModel.create({
                 type: 'withdraw',
                 amount: detail.amount,
-                account: detail.account,
+                senderId: detail.senderId,
+                content: detail.content,
+                status: 'pending'
             });
             // send notification to admin
             return response;
@@ -20,7 +23,7 @@ class TransactionService {
     static getListTransactionByUserId = async (query, userId) => {
         try {
             const response = await transactionModel
-                .find({ senderId: userId })
+                .find({ senderId: userId, type: 'payArtWork' })
                 .limit(query.pageSize)
                 .skip((query.currentPage - 1) * query.pageSize)
                 .populate('senderId', 'name')
@@ -40,7 +43,7 @@ class TransactionService {
     static getListTransaction = async (query) => {
         try {
             const response = await transactionModel
-                .find()
+                .find({ type: 'payArtWork' })
                 .limit(query.pageSize)
                 .skip((query.currentPage - 1) * query.pageSize)
                 .populate('senderId', 'name')
@@ -54,6 +57,61 @@ class TransactionService {
                 currentPage: query.currentPage,
             };
         } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    static getListWithdrawByAdmin = async (query) => {
+        try {
+            const result = await transactionModel
+                .find({ type: 'withdraw' })
+                .limit(query.pageSize)
+                .skip((query.currentPage - 1) * query.pageSize)
+                .populate('senderId', 'name')
+            // count total page
+            const totalPage = Math.ceil(await transactionModel.countDocuments({ type: 'withdraw' }) / query.pageSize);
+            return {
+                result,
+                totalPage,
+                pageSize: query.pageSize,
+                currentPage: query.currentPage,
+            };
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    static getListWithdrawByUser = async (query, senderId) => {
+        try {
+            const result = await transactionModel
+                .find({ type: 'withdraw', senderId: senderId })
+                .limit(query.pageSize)
+                .skip((query.currentPage - 1) * query.pageSize)
+                .populate('senderId')
+            // count total page
+            const totalPage = Math.ceil(await transactionModel.countDocuments({ type: 'withdraw' }) / query.pageSize);
+            return {
+                result,
+                totalPage,
+                pageSize: query.pageSize,
+                currentPage: query.currentPage,
+            };
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    static updateWithdrawStatus = async (id, status) => {
+        try {
+            const transaction = await transactionModel.findById(id);
+            if (transaction.type !== 'withdraw') {
+                throw new Error(`This transaction can't edit `);
+            }
+            // update status
+            const update = await transactionModel.findByIdAndUpdate(id, { status: status });
+            return update;
+        }
+        catch (error) {
             throw new Error(error.message);
         }
     }
