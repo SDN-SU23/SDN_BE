9366e2f4a58ca68a1dd00f3f6e2f7c9797dec044
Huynh Chi Bao <baohc110902@gmail.com>
2024-03-17 14:09:40 +0700

    Refactor transaction route URLs

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -5,13 +5,13 @@ const { checkIsLogin, checkRole } = require('../auth');
 
 // router.use(checkIsLogin)
 
-router.get('/:userId', transactionController.getListTransactionByUserId);
+router.get('/getListByUser/:userId', transactionController.getListTransactionByUserId);
 
 router.post('/withdraw', transactionController.withDraw);
 
 // router.use(checkRole('Admin'))
 
-router.get('/admin', transactionController.getListTransaction);
+router.get('/getListByAdmin', transactionController.getListTransaction);
 
 router.put('/:transactionId', transactionController.updateTransaction);
 
