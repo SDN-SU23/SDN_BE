983b7bed2c49c54e53c91969bb49796c193f5ddf
Huynh Chi Bao <baohc110902@gmail.com>
2024-03-18 13:08:53 +0700

    Fix function name in getNotificationByuser method

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -5,7 +5,7 @@ const NotificationService = require("../services/notification.service");
 class NotificationController {
   getNotificationByuser = async (req, res) => {
     try {
-      const notification = await NotificationService.s(req.params.userId);
+      const notification = await NotificationService.getNotificationByuser(req.params.userId);
       res.status(200).json(notification);
     } catch (error) {
       res.status(500).json({ message: error.message });
